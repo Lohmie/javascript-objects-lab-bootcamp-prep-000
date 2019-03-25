e4ec9986eabaff9157c5b4d449dd48c00a3d452a
Mariam <mariamjhabib@gmail.com>
2019-03-25 15:31:24 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+var recipes = {};
+
+function recipes(Object) {
+  return Object;
+}
+
+function updateObjectWithKeyAndValue(Object, key, value) {
+  var Obj = {};
+  obj[""] = "key: value";
+  
+  return Obj;
+}
